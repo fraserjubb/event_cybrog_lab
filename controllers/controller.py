3ecc457e15da5713ac12d3bc09e2256cc9bc0dda
Fraser Jubb <fraser.jubb@gmail.com>
2022-12-08 15:29:35 +0000

    Finished mvp

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,16 @@ from models.event import Event
 @app.route('/events')
 def index():
     return render_template('index.html', title='Home', events=events)
+
+@app.route('/events', methods = ['POST'])
+def create():
+    date = request.form["date"]
+    name = request.form["name"]
+    number_of_guests = request.form["number_of_guests"]
+    location = request.form["location"]
+    description = request.form["description"]
+    new_event = Event(date, name, number_of_guests, location, description)
+    events.append(new_event)
+    # return render_template('index.html', title='Home', events=events)
+
+    return redirect('/events')
